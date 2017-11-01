8fabdc9d65703ea491c1c3fd2cb10b8a87cadc29
Arneld van der Veen <ae.van.der.veen@st.hanze.nl>
2017-11-01 15:45:40 +0100

    test

--- Test Ultrasonor/Ultrasonorsensor/main.c
+++ Test Ultrasonor/Ultrasonorsensor/main.c
@@ -1,288 +1,89 @@
-//
-// Talking to ultrasonic sensor HC-SR04 with an ATtiny84, and
-// sending distance data using serial communications.
-//
-// electronut.in
-//
-
 #include <avr/io.h>
-#include <string.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-#define F_CPU 8000000
+void Send_signal(void);
+void Initialize_external_interrupt (void);
+void Initialize_timer0 (void);
 
-//
-// BEGIN: serials comms
-//
+unsigned char working;
+unsigned char rising_edge;
+uint16_t timer_value;
+int distance_cm;
+uint8_t error;
 
-char pinTX = PINB1;
-volatile unsigned char currByte = 0;
-volatile int sendingData = 0;
-
-// initialize
-void init_serial()
+ISR (TIMER0_OVF_vect)
 {
-	// set TX pin as output
-	DDRB = (1 << pinTX);
+	if(rising_edge==1) //Check if there was echo
+	{
+		timer_value++;
+		/*Check if isnt out of range*/
+		if(timer_value > 91)
+		{
+			working = 0;
+			rising_edge = 0;
+			error = 1;
+		}
+	}
+}
+ISR (INT1_vect)
+{
+	if(working==1) //Check if echo is high, start timer
+	{
+		if(rising_edge==0)
+		{
+			rising_edge=1;
+			TCNT0 = 0;
+			timer_value = 0;
+		}
+		else //Check if echo turned low, calculate distance
+		{
+			rising_edge = 0;
+			distance_cm = (timer_value*256 + TCNT0)/58;
+			working = 0;
+		}
+	}
+}
 
-	// set up timer1:
-	cli();
-	// 16 bit timer
-	// Divide by 1
-	TCCR1B |= (1<<CS10);
-	// Count cycles - (1/9600)*8000000
-	OCR1A = 833;
-	// Put Timer/Counter1 in CTC mode
-	TCCR1B |= 1<<WGM12;
-	// set interrupt flag
-	TIMSK1 |= 1<<OCIE1A;
+int main(void)
+{
+	/*Initializing interrupts and screen */
+	Initialize_external_interrupt();
+	Initialize_timer0();
+	DDRB |=  (1 << PINB1);
+	DDRD &=~ (1 << PIND3);
+	DDRD |= (1 << PIND4);
 	sei();
-}
-
-// send a byte
-void send_byte(char data)
-{
-	// set flag
-	sendingData = 1;
-	// set current data byte
-	currByte = data;
-	// wait till done
-	while(sendingData);
-}
-
-// which bit was sent last? (0-10)
-// -1 implies none sent
-volatile char bitNum = -1;
-
-// 16-bit timer CTC handler - Interrupt Service Routine
-ISR(TIM1_COMPA_vect)
-{
-	// serial data sent as:
-	//
-	// 9600 8 N 1
-	//
-	// start(low)-0-1-2-3-4-5-6-7-stop(high)-idle(high)-idle(high)
-	// 12 bits sent per packet
-
-	// idle => high
-	if(!sendingData) {
-		PORTB |= (1 << pinTX);
-	}
-	else {
-
-		if(bitNum < 0) {
-			// start bit - low
-			PORTB &= ~(1 << pinTX);
-			// set bit number
-			bitNum = 0;
-		}
-		else if(bitNum >= 8) {
-			// stop bit - high
-			PORTB |= (1 << pinTX);
-			// increment
-			bitNum++;
-
-			// send 2 idle - high - bits
-			// not necessary strictly speaking
-			if(bitNum == 10) {
-				// done
-				sendingData = 0;
-				// unset bit number
-				bitNum = -1;
-			}
-		}
-		else {
-			// data bits:
-
-			// bit num is in [0, 7]
-
-			// extract relevant bit from data
-			char dataBit = currByte & (1 << bitNum);
-			if(dataBit) {
-				PORTB |= (1 << pinTX);
-			}
-			else {
-				PORTB &= ~(1 << pinTX);
-			}
-
-			// update bit number
-			bitNum++;
-		}
+	while(1)
+	{
+		Send_signal(); //Restarting for another conversation
 	}
 }
 
-
-// write null terminated string
-void send_str(const char* str)
+void Send_signal()
 {
-	int len = strlen(str);
-	int i;
-	for (i = 0; i < len; i++) {
-		send_byte(str[i]);
+	if(working ==0) //Be sure that conversation is finished
+	{
+		_delay_ms(50);		//Restart HC-SR04
+		PORTD &=~ (1 << PIND4);
+		_delay_us(1);
+		PORTD |= (1 << PIND4); //Send 10us second pulse
+		_delay_us(10);
+		PORTD &=~ (1 << PIND4);
+		working = 1;	//Be sure that it is ready
+		error = 0;		//Clean errors
 	}
 }
 
-//
-// END: serials comms
-//
-
-
-//
-// BEGIN: HC-SR04 code
-//
-// pins for HC-SR04
-int pinEcho = PIND0;
-
-// initialize HC-SR04
-void initHCSR04()
+void Initialize_external_interrupt()
 {
-	// initialize HC-SR04 pins
-	
-	// set Trigger pin (connected to PB0) as output
-	DDRB |= (1 << PINB0);
+	MCUCR |= (1 << ISC10); //Any logical change on INT1
+	GICR |= (1 << INT1); //Enable INT1
 }
 
-// 1/0 flag to check if echo is over
-volatile char echoDone = 0;
-
-// current timer0 count
-uint32_t countTimer0 = 0;
-
-// get distance in cm from HC-SR04
-float getDistanceHCSR04()
+void Initialize_timer0()
 {
-	// Send a 10us HIGH pulse on the Trigger pin.
-	// The sensor sends out a “sonic burst” of 8 cycles.
-	// Listen to the Echo pin, and the duration of the next HIGH
-	// signal will give you the time taken by the sound to go back
-	// and forth from sensor to target.
-
-	float distance = 0.0f;
-
-	// enable pin-change interrupt on PCINT0:
-	cli();
-	// enable interrupt
-	GIMSK |= (1 << PCIE0);
-	// enable pin
-	PCMSK0 |= (1 << PCINT0);
-	sei();
-
-	// set echo flag
-	echoDone = 0;
-	// reset counter
-	countTimer0 = 0;
-
-	// send 10us trigger pulse
-	//    _
-	// __| |__
-	PORTB &= ~(1 << PINB0);
-	_delay_us(20);
-	PORTB |= (1 << PINB0);
-	_delay_us(12);
-	PORTB &= ~(1 << PINB0);
-	_delay_us(20);
-
-	// listen for echo and time it
-	//       ____________
-	// _____|            |___
-
-	// loop till echo pin goes low
-	while(!echoDone);
-
-	// disable pin-change interrupt:
-	// disable interrupt
-	GIMSK &= ~(1 << PCIE0);
-	// disable pin
-	PCMSK0 &= ~(1 << PCINT0);
-
-	// calculate duration
-	float duration = countTimer0/8000000.0;
-
-	// dist = duration * speed of sound * 1/2
-	// dist in cm = duration in s * 340.26 * 100 * 1/2
-	// = 17013*duration
-	distance = 17013.0 * duration;
-	
-	return distance;
-}
-
-// timer0 overflow interrupt
-ISR(TIM0_OVF_vect)
-{
-	// increment
-	countTimer0 += 255;
-}
-
-// pin-change interrupt handler
-ISR(PCINT0_vect)
-{
-	// read PCINT0 (PA0 - pn 13):
-	if(PINA & (1 << pinEcho)) {
-		// rising edge:
-
-		// start 8-bit timer
-		// Divide by 1
-		TCCR0B |= (1<<CS00);
-		// set overflow interrupt flag
-		TIMSK0 |= 1<<TOIE0;
-
-	}
-	else {
-		// falling edge
-
-		// stop timer
-		TCCR0B &= ~(1<<CS00);
-
-		// calculate time passed
-		countTimer0 += TCNT0;
-
-		// reset counter in timer0
-		TCNT0 = 0;
-
-		// set flag
-		echoDone = 1;
-	}
-}
-//
-// END: HC-SR04 code
-//
-
-
-//
-// main loop:
-//
-int main (void)
-{
-	// serial
-	init_serial();
-
-	// HC-SR04
-	initHCSR04();
-
-	// set as output
-	DDRB |= (1 << PB0);
-
-	char str[16];
-
-	float prevDist = 0.0;
-	// loop
-	while (1) {
-
-		float dist = getDistanceHCSR04();
-		// sensor only works till 400 cm - if it exceeds, this value
-		// just send previous reading
-		if(dist > 500) {
-			dist = prevDist;
-		}
-		// print distance to serial port
-		sprintf(str, "%f\n", dist);
-		send_str(str);
-		prevDist = dist;
-
-		// wait
-		_delay_ms(70);
-	}
-	
-	return 1;
+	TCCR0 |= (1 << CS00); //No prescaling
+	TCNT0 = 0;			//Reset timer
+	TIMSK |= (1 << TOIE0); //Timer overflow interrupt enable
 }
