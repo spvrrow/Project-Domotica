b63b1e9143ec34f1fb8ac40628f97f7cec1923f2
Arneld van der Veen <ae.van.der.veen@st.hanze.nl>
2017-11-07 12:01:29 +0100

    Serial connectie
    Serial connectie werkt met sturen van String

--- Eindproduct/Eindproduct/main.c
+++ Eindproduct/Eindproduct/main.c
@@ -1,52 +1,43 @@
           /*
  * Eindproduct.c
  *
- * ultrasonoor measures distance between 2 and 70 cm
+ * Eindproduct voor het project zonneschermen
  *
- * Created: 29-6-2016 14:44:43
- *  Author: Arneld & Antonio Soprano
+ * Created: 20-10-2017 14:44:43
+ *  Author: ICT-MAX
  */ 
 
 /* 
  * HC-SR04
  * trigger to sensor : uno 4 (PD4) output
  * echo from sensor  : uno 3 (PD3 = INT1) input
- * DIO : uno 8  (PB0) data
- * CLK : uno 9  (PB1) clock
- * STB : uno 10 (PB2) strobe
  */
+
+/* output on USB = PD1 = board pin 1
+*F_OSC = 16 MHz & baud rate = 19.200
+*/
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #define F_CPU 16E6
 #include <util/delay.h>
+#include <avr/sfr_defs.h>
 #include "distance.h"
-#define BAUD	9600
-#define BRC		((F_CPU/16/BAUD)-1)
+
+#define UBBRVAL 51
 
 volatile uint16_t gv_counter; // 16 bit counter value
 volatile uint8_t gv_echo; // a flag
-volatile unsigned char value;  
 
-int main(void)
-{
-    
-	USART_Init();
-	sei();
-	value = 'A';
-
-	for(;;){
-		
-		USART_SendByte(value);
-		_delay_ms(250);
-}
-
+// Port initialization
 void init_ports(void)
 {
 	DDRD = (1 << 4);
 	DDRD = ~(1 << 3);
 	
 }
+
+// Initialization of timers
 void init_timer(void)
 // prescaling : max time = 2^16/16E6 = 4.1 ms, 4.1 >> 2.3, so no prescaling required
 // normal mode, no prescale, stop timer
@@ -55,6 +46,7 @@ void init_timer(void)
 	TCCR1B = 0;
 }
 
+// External interrupts initialization
 void init_ext_int(void)
 {
 	// any change triggers ext interrupt 1
@@ -63,12 +55,13 @@ void init_ext_int(void)
 }
 
 
+// Start of 
 uint16_t calc_cm(uint16_t counter)
 {
 	return (gv_counter / 16)/58;
 }
 
-int main(void)
+int afstand_meter(void)
 {
 	init_ports();
 	init_timer();
@@ -102,29 +95,58 @@ ISR (INT1_vect)
 	}
 }
 
-volatile unsigned char value;
+// Start of Serial Code
 
-ISR (USART_RXC_vect){
-	value = UDR0;
-	PORTB = ~value;
+void uart_init()
+{
+	// set the baud rate
+	UBRR0H = 0;
+	UBRR0L = UBBRVAL;
+	// disable U2X mode
+	UCSR0A = 0;
+	// enable transmitter
+	UCSR0B = _BV(TXEN0);
+	// set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
 }
+void transmit(uint8_t data)
+{
+	// wait for an empty transmit buffer
+	// UDRE is set when the transmit buffer is empty
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	// send the data
+	UDR0 = data;
+}
+char String[]="The distance is:";
 
-void USART_Init(void){
+void USART_putstring(char* StringPtr){
 	
-	UBRR0L = BRC;//   UBRR0H = (BRC >> 8);
-	UCSR0B = ((1<<TXEN0)|(1<<RXEN0) | (1<<RXCIE0));
-}
-
-void USART_SendByte(uint8_t u8Data){
-
+	while(*StringPtr != 0x00){ //Here we check if there is still more chars to send, this is done checking the actual char and see if it is different from the null char
+		transmit(*StringPtr);//Using the simple send function we send one char at a time
+	StringPtr++;}        //We increment the pointer so we can read the next char
 	
-	while((UCSR0A &(1<<UDRE0)) == 0);
-
-	UDR0 = u8Data;
 }
 
-uint8_t USART_ReceiveByte(){
-	while((UCSR0A &(1<<RXC0)) == 0);
+unsigned char USART_receive(void){
+	
+	while(!(UCSR0A & (1<<RXC0)));
 	return UDR0;
+	
 }
 
+int serial_conn(void){
+	uart_init();
+	while (1) {
+		USART_putstring(String);
+		_delay_ms(5000);
+	}
+	return 0;
+}
+
+
+
+int main(void)
+{
+	serial_conn();
+	afstand_meter();
+}
