501ea6dddfeb45a6bdcaaf6b2627b17d8fa3fa7a
Arneld van der Veen <ae.van.der.veen@st.hanze.nl>
2017-11-08 16:28:27 +0100

    photocell working

--- Test fotocel/Test Photocel/main.c
+++ Test fotocel/Test Photocel/main.c
@@ -7,11 +7,17 @@
 
 #include <avr/io.h>
 #define F_CPU 16E6 // used in _delay_ms, 16 MHz
-#include <util/delay.h>
+#include <util/delay.h>#include <avr/interrupt.h>char String[]="D,";
 
+//Defines
+#define UBBRVAL 51
 
 int main(void)
 {
+	init_adc();
+	serial_conn();
+}
+
 void init_adc()
 {
 	// ref=Vcc, left adjust the result (8 bit resolution),
@@ -25,6 +31,58 @@ uint8_t get_adc_value()
 	ADCSRA |= (1<<ADSC); // start conversion
 	loop_until_bit_is_clear(ADCSRA, ADSC);
 	return ADCH; // 8-bit resolution, left adjusted
+}
+
+// Start of Serial Code
+
+void uart_init()
+{
+	// set the baud rate
+	UBRR0H = 0;
+	UBRR0L = UBBRVAL;
+	// disable U2X mode
+	UCSR0A = 0;
+	// enable transmitter
+	UCSR0B = _BV(TXEN0);
+	// set frame format : asynchronous, 8 data bits, 1 stop bit, no parity
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+}
+
+void transmit(uint8_t data)
+{
+	// wait for an empty transmit buffer
+	// UDRE is set when the transmit buffer is empty
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	// send the data
+	UDR0 = data;
 }
+void USART_putstring(char* StringPtr){
+	
+	while(*StringPtr != 0x00){ //Here we check if there is still more chars to send, this is done checking the actual char and see if it is different from the null char
+		transmit(*StringPtr);//Using the simple send function we send one char at a time
+	StringPtr++;}        //We increment the pointer so we can read the next char
+	
+}
+
+unsigned char USART_receive(void){
+	
+	while(!(UCSR0A & (1<<RXC0)));
+	return UDR0;
+	
+}
+
+int serial_conn(void){
+	uart_init();
+	while (1) {
+		USART_putstring(String);
+		//convert int to string
+		char buffer[8];
+		int tmp = get_adc_value();
+		itoa(tmp, buffer, 10);
+		USART_putstring(buffer);
+		//USART_putstring(String2);
+		_delay_ms(3000);
+	}
+	return 0;
 }
 
