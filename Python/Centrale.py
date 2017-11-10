8b54ac8a9466d19f0143ed67232335d55a3bd1be
DanielBouius <d.bouius@st.hanze.nl>
2017-11-10 18:47:17 +0100

    Aanpassingen

--- Python/Centrale.py
+++ Python/Centrale.py
@@ -9,13 +9,14 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
+from PIL import ImageTk, Image
 import matplotlib
 import serial
 import matplotlib.pyplot as plt
 import matplotlib as animation
 matplotlib.use("TkAgg")
 from drawnow import *
-from time import sleep
+from threading import Thread
 
 
 
@@ -33,20 +34,15 @@ inrolstand = 2
 standaard_inrol = 2
 uitrolstand = 10
 standaard_uitrol = 10
-Licht = []
-lightF = []
 
 #Maken de class voor de GUI
 class Window(tk.Tk):
     def __init__(self,):
         tk.Tk.__init__(self,)
 
-
-        self.SerialArduino = serial.Serial('COM1', 19200)
-
-
-
-
+        lightF = []
+        plt.ion()
+        self.SerialArduino = serial.Serial('COM1', 9600)
 
         #status
         self.statusLabel = Label(self, text= "status rolluik: niet bekend", font=5)
@@ -119,7 +115,7 @@ class Window(tk.Tk):
         self.licht_bovenWaardeEntry = Entry(self)
         self.licht_bovenWaardeEntry.insert(0, licht_bovengrens)
         self.licht_bovenWaardeEntry.place(x=500, y=720, width=80)
-        self.licht_bovenWaardeButton = Button(self, text= "zet", command=self.getLicht)
+        self.licht_bovenWaardeButton = Button(self, text= "zet", command=self.set_lichtBoven)
         self.licht_bovenWaardeButton.place(x=600, y= 720)
         self.licht_bovenWaardeLabel = Label(self, text="Bovenwaarde:")
         self.licht_bovenWaardeLabel.place(x=416, y= 718)
@@ -134,7 +130,7 @@ class Window(tk.Tk):
             plt.title("Licht data")
             plt.grid(True)
             plt.ylabel("licht")
-            plt.plot()
+            plt.plot([5, 2, 1, 5, 6, 7, 8,500 ])
             plt.legend(loc='upper left')
             plt2 = plt.twinx()
             plt.ylim(0, 50)
@@ -143,25 +139,33 @@ class Window(tk.Tk):
             plt2.ticklabel_format(usedOffset=False)
             plt2.legend(loc='upper right')
 
+    #Functie voor het plotten en calculeren van de grafiek
+    def draw(self):
+        self.getLicht()
+        global Licht
+        drawnow(self.makeFig)
+        plt.pause(.00001)
+        cnt = 0
+        cnt = cnt + 1
+        if(cnt>50):
+            Licht.pop(0)
 
 
     def getLicht(self):
+
         global Licht
         while True:
             while (self.SerialArduino.inWaiting() == 0):  # Wait here until there is data
                 pass  # do nothing
-
             valueRead = self.SerialArduino.readline()
             dataArray = valueRead.decode().split(',')
             if (dataArray[0] == "L"):
                 temp = int(dataArray[1])  # Convert first element to floating number and put in temp
                 Licht.append(temp)  # Build our tempF array by appending temp readings
                 print(Licht)
-
-    #Functie voor het plotten en calculeren van de grafiek
-    def draw(self):
-        
-            drawnow(self.makeFig)
+            t1 = Thread(target=self.getLicht)
+            t1.start()
+            #root.after(2000, self.draw)
 
     # Hieronder staan de button functies
     def set_lichtBoven(self):
@@ -186,10 +190,9 @@ class Window(tk.Tk):
             ondergrens = int(self.licht_onderWaardeEntry.get())
             if ondergrens < licht_bovengrens:
                 licht_ondergrens = ondergrens
-                ondergrens_send = ondergrens
+                ondergrens_send = ("u" + str(ondergrens))
                 print(ondergrens_send)
-
-                self.SerialArduino.write((b'ondergrens_send'))
+                self.SerialArduino.write(ondergrens_send)
             else:
                 self.licht_onderWaardeEntry.delete(0, tk.END)
                 self.licht_onderWaardeEntry.insert(tk.INSERT, standaard_ondergrensL)
@@ -270,21 +273,9 @@ class Window(tk.Tk):
         #funcite toevoegen voor het verwijderen van de rolluiken
         self.statusLabel.config(text="status rolluik: is gesloten")
 
-
-
-
-
-
-
-
-
-
-
-
 root = Window()
-
 root.title("Centrale")
-
+#root.after(1000, root.draw)
 root.geometry("1000x800")
 root.mainloop()
 
